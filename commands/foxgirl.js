0692a8c06879de2c7eabacbb9ab495a923c1f2b1
icedrake45 <icedrake45@gmail.com>
2022-10-24 00:26:01 -0700

    Replaces akaneko library with hmfull
    Wraps all the command collection and event listener code in functions in index.js

--- commands/foxgirl.js
+++ commands/foxgirl.js
@@ -1,9 +1,13 @@
 import { SlashCommandBuilder } from 'discord.js';
-import akaneko from 'akaneko';
+import hmfull from 'hmfull';
 
+/**
+ * https://www.npmjs.com/package/hmfull
+ */
 export const data = new SlashCommandBuilder()
                     .setName('foxgirl')
                     .setDescription('SFW - Fox Girls');
 export async function execute(interaction) {
-    await interaction.reply(akaneko.foxgirl());
+    const foxgirl = await hmfull.Nekos.sfw.foxgirl();
+    await interaction.reply(foxgirl.url);
 };
--- index.js
+++ index.js
@@ -14,33 +14,57 @@ client.commands = new Collection();
 /**
  * Add all the commands from commands/ into the Collection
  */
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+async function collectCommands() {
+	try {
+		const commandsPath = path.join(__dirname, 'commands');
+		const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
+		for (const file of commandFiles) {
+			const filePath = path.join(commandsPath, file);
+			const command = await import(filePath);
 
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+			// Set a new item in the Collection with the key as the command name and the value as the exported module
+			if ('data' in command && 'execute' in command) {
+				client.commands.set(command.data.name, command);
+			} else {
+				console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+			}
+		}
+	} catch (error) {
+		throw error;
 	}
 }
 
 /**
  * Register event listeners for each event in events/
  */
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+async function registerClientEventListeners() {
+	try {
+		const eventsPath = path.join(__dirname, 'events');
+		const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
+		for (const file of eventFiles) {
+			const filePath = path.join(eventsPath, file);
+			const event = await import(filePath);
+			if (event.once) {
+				client.once(event.name, (...args) => event.execute(...args));
+			} else {
+				client.on(event.name, (...args) => event.execute(...args));
+			}
+		}
+	} catch (error) {
+		throw error;
 	}
 }
+
+async function clientLogin() {
+	try {
+		await collectCommands();
+		await registerClientEventListeners();
+		client.login(process.env.DISCORD_TOKEN);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+clientLogin();
